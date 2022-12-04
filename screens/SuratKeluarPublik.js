ecfbfb5f47035f315c74855efc2669167141e8b9
e-use <114447669+yusril-iskandar-harahap@users.noreply.github.com>
2022-12-04 22:27:39 +0700

    Update SuratKeluarPublik.js

--- screens/SuratKeluarPublik.js
+++ screens/SuratKeluarPublik.js
@@ -1,11 +1,23 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
+import { Table, Row, Rows } from 'react-native-table-component';
 
 export default function SuratKeluarPublik({ navigation }) {
 
+  const header = ['heading 1', 'heading 2', 'heading 3']
+  const data = [
+      ['gfg1', 'gfg2', 'gfg3'],
+      ['gfg4', 'gfg5', 'gfg6'],
+      ['gfg7', 'gfg8', 'gfg9']
+
+  ]
   return (
-    <View style={styles.container}>
-      <Text>Ini Surat Keluar Publik</Text> 
+    <View style={{marginTop: 200}}>
+      <Text style={{ fontSize: 18}}>Tabel Surat Keluar</Text>
+      <Table borderStyle={{ borderWidth: 2, 
+          borderColor: '#c8e1ff' }}>
+          <Row data={header} />
+          <Rows data={data} />
+      </Table>
     </View>
   );
 }
